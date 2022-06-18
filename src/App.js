119db25d6c80d5653749a02ab5708f53702c70cf
Mattias Gaspar <mattiasmac2000@gmail.com>
2022-06-18 15:45:56 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-function App() {
-    let data = new Date()
-
+function App1() {
     let resultsTxt = document.querySelector('main h1')
     resultsTxt.innerHTML = results()
 
     function results() {
-        return `${getDayWeek(data.getDay())}, ${getDayMonth(data.getDate())} de ${getMonth(data.getMonth())} de ${getYear(data.getFullYear())} <br> ${getHour(data.getHours(), data.getMinutes())}`
+        const data = new Date()
+
+        return `${getDayWeek(data.getDay())}, ${getDayMonth(data.getDate())} de ${getMonth(data.getMonth())} de ${getYear(data.getFullYear())} <br> ${getHour(data.getHours())}:${getMinute(data.getMinutes())}`
     }
 
     function getDayWeek(day) {
@@ -28,10 +28,116 @@ function App() {
         return year
     }
 
-    function getHour(hour, minute) {
-        return minute < 10 ? `${hour}:0${minute}` : `${hour}:${minute}`
+    function getHour(hour) {
+        return hour < 10 ? `0${hour}` : `${hour}`
+    }
+
+    function getMinute(minute) {
+        return minute < 10 ? `0${minute}` : `${minute}`
     }
 
 }
 
-App()
+function App2() {
+    let resultsTxt = document.querySelector('main h1')
+    resultsTxt.innerHTML = results()
+
+    function results() {
+        const data = new Date()
+
+        return `${getDayWeek(data.getDay())}, ${getDayMonth(data.getDate())} de ${getMonth(data.getMonth())} de ${getYear(data.getFullYear())} <br> ${getHour(data.getHours())}:${getMinute(data.getMinutes())}`
+    }
+
+    function getDayWeek(day) {
+        switch(day) {
+            case 0:
+            return 'domingo'
+
+            case 1:
+            return 'segunda-feira'
+
+            case 2:
+            return 'terça-feira'
+
+            case 3:
+            return 'quarta-feira'
+
+            case 4:
+            return 'quinta-feira'
+
+            case 5:
+            return 'sexta-feira'
+
+            case 6:
+            return 'sábado'
+        }
+    }
+
+    function getDayMonth(date) {
+        return date
+    }
+
+    function getMonth(month) {
+        switch(month) {
+            case 0:
+            return 'Janeiro'
+
+            case 1:
+            return 'Fevereiro'
+
+            case 2:
+            return 'Março'
+
+            case 3:
+            return 'Abril'
+
+            case 4:
+            return 'Maio'
+
+            case 5:
+            return 'Junho'
+
+            case 6:
+            return 'Julho'
+
+            case 7:
+            return 'Agosto'
+
+            case 8:
+            return 'Setembro'
+
+            case 9:
+            return 'Outubro'
+
+            case 10:
+            return 'Novembro'
+
+            case 11:
+            return 'Dezembro'
+        }
+    }
+
+    function getYear(year) {
+        return year
+    }
+
+    function getHour(hour) {
+        return hour < 10 ? `0${hour}` : `${hour}`
+    }
+
+    function getMinute(minute) {
+        return minute < 10 ? `0${minute}` : `${minute}`
+    }
+
+}
+
+function App3() {
+    const resultsTxt = document.querySelector('main h1')
+    const data = new Date()
+
+    resultsTxt.innerHTML = Intl.DateTimeFormat('pt-BR', { dateStyle: 'full', timeStyle: 'short' }).format(new Date())
+
+    resultsTxt.innerHTML += `<br> ${data.toLocaleString('pt-BR', { dateStyle: 'full', timeStyle: 'short' })}`
+}
+
+App3()
